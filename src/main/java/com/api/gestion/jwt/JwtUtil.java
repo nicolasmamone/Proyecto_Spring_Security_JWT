2766dbf0fb2a82bc009b7763bc5f50186b42416e
Nico Mamone <nicolasmamone@gmail.com>
2024-01-08 19:20:25 -0300

    Creación e implementación de la clase JwtUtil

---
+++ src/main/java/com/api/gestion/jwt/JwtUtil.java
@@ -0,0 +1,64 @@
+package com.api.gestion.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+
+    private String secret = "springboot";
+
+    private String extractUserName(String token){
+        return extractClaims(token,Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token){
+        //firmando el token con una clave secreta
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date()); // si la fecha de exp es anterior a la de ahora entonces esta exp
+    }
+
+    public String generateToken(String username, String role){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+        return createToken(claims, username);
+    }
+
+    public String createToken(Map<String, Object> claims, String subject){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 100 * 60 * 60 * 10 ))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    //Si el token no ha expirado y el nombre de usuario coincide entonces true
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+}
